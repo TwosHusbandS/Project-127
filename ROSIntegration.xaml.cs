a7ffc0d33d8622b3f3b9c2e7fd02bcc688211de9
jaredtb <jaredtb00@gmail.com>
2020-09-11 09:53:54 -0500

    Begining of ROS-CEF integration (not currently working)

---
+++ ROSIntegration.xaml.cs
@@ -0,0 +1,36 @@
+﻿using CefSharp;
+using CefSharp.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Project_127
+{
+    /// <summary>
+    /// Interaction logic for ROSIntegration.xaml
+    /// </summary>
+    public partial class ROSIntegration : Window
+    {
+        public ROSIntegration()
+        {
+            InitializeComponent();
+        }
+        private void LoadingStateChange(object sender, LoadingStateChangedEventArgs args)
+        {
+            if (!args.IsLoading)
+            {
+                browser.ExecuteScriptAsync("alert('All Resources Have Loaded');");
+            }
+        }
+    }
+}
