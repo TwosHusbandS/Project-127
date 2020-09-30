e5c78ee22e7baea50e2d6c414634645ffcf58910
Ingo Wolfgang <ingo.wolfgang@t-online.de>
2020-09-30 15:06:05 +0200

    Push so I can merge with dragons branch. WIP. FIXED

---
+++ ProcessSpawner127/ProcessSpawner127/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ProcessSpawner127
+{
+	/// <summary>
+	/// This will be used as a ProcessSpawner.
+	/// If this doesnt work, I will use this to spawn project 1.27 itself and then to sapwn gtav and stuff
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// Main Method
+		/// </summary>
+		/// <param name="args"></param>
+		static void Main(string[] args)
+		{
+			// Fully finished, documented, tested, integrated, and finalized Code below
+
+			string FilePath = "";
+			string Arguments = "";
+			string WorkingDir = "";
+			bool runAsAdmin = false;
+
+			for (int i = 0; i <= args.Length - 1; i++)
+			{
+				Console.WriteLine("Arg: {0} = '{1}'", i, args[i]);
+			}
+			Console.WriteLine("Done with that ");
+			Console.ReadKey();
+		}
+	}
+}
