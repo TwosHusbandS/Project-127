ad160b0bd25805ce0277727fd5f096c056841c01
jaredtb <jaredtb00@gmail.com>
2021-01-09 08:14:58 -0600

    Removed some notimplemented stuff

--- Overlay/NoteOverlay.xaml.cs
+++ Overlay/NoteOverlay.xaml.cs
@@ -543,12 +543,12 @@ namespace Project_127.Overlay
 
 		public static void OverlayNoteChapterNext()
 		{
-			throw new NotImplementedException();
+			MyGTAOverlay.nextChapter();
 		}
 
 		public static void OverlayNoteChapterPrev()
 		{
-			throw new NotImplementedException();
+			MyGTAOverlay.prevChapter();
 		}
 
 
