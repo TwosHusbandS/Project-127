024875b53f351b7ec54c986bfeede4adc54a8f11
jaredtb <jaredtb00@gmail.com>
2021-01-18 13:30:46 -0600

    Some easter stuff

--- Overlay/NoteOverlay.xaml.cs
+++ Overlay/NoteOverlay.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using Project_127.HelperClasses;
 using Project_127.MySettings;
 using System.Diagnostics;
+using System.Windows.Threading;
 
 namespace Project_127.Overlay
 {
@@ -733,6 +734,8 @@ namespace Project_127.Overlay
 
 		bool waitingForSecondRightClick = false;
 
+		private static OverlayAnimationObject ast;
+		private static DispatcherTimer dtast;
 		private static async void easterEgg()
 		{
 			// We are very serious people...
@@ -740,7 +743,7 @@ namespace Project_127.Overlay
 			{
 				//return;
 			}
-			var ast = new OverlayAnimationObject("rick");
+			ast = new OverlayAnimationObject("rick");
 			MyGTAOverlay.attach(ast);
 			await Task.Run(async () =>
 			{
@@ -752,10 +755,29 @@ namespace Project_127.Overlay
 				//var richard = System.Windows.Application.GetResourceStream(new Uri(@"Overlay\richard.gif", UriKind.Relative));
 				//await ast.loadGif(System.Drawing.Image.FromStream(richard.Stream));
 				ast.fillOverlay = true;
-				ast.visible = true;
+				ast.visible = false;
 				ast.FPS = 10;
 				ast.opacity = .5F;
+				dtast = new System.Windows.Threading.DispatcherTimer();
+				dtast.Tick += new EventHandler(easterEggLoop);
+				dtast.Interval = TimeSpan.FromMilliseconds(2500);
+				dtast.Start();
 			});
+		}
+
+		public static void easterEggLoop(object sender, EventArgs e)
+		{
+			float percent = 0;
+            try
+            {
+				percent = Globals.GTAPointerPathHandler.EvalPointerPath_fp32(Globals.stateVarsCurrent["percent"]);
+			}
+            catch { }
+			if (percent == 100)
+            {
+				ast.visible = true;
+            }
+
 		}
 
 		private async void btn_Looks_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
