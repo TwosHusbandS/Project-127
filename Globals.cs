ac566f49484523ab19d3e6c5d68ac04bc02f2275
Ingo Wolfgang <ingo.wolfgang@t-online.de>
2021-06-22 16:28:54 +0200

    1.2.3.0 RC 2

--- Globals.cs
+++ Globals.cs
@@ -230,7 +230,7 @@ namespace Project_127
 		/// <summary>
 		/// Property of other Buildinfo. Will be in the top message of logs
 		/// </summary>
-		public static string BuildInfo = "1.2.3.0 - RC 1";
+		public static string BuildInfo = "1.2.3.0 - RC 2";
 
 
 		/// <summary>
