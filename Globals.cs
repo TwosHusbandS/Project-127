95ea30ebd41a2a93e89602da2ebc617b73c8f951
Ingo Wolfgang <ingo.wolfgang@t-online.de>
2024-05-18 17:49:30 +0200

    Installer build 2 sclbeta

--- Globals.cs
+++ Globals.cs
@@ -235,7 +235,7 @@ namespace Project_127
 		/// <summary>
 		/// Property of other Buildinfo. Will be in the top message of logs
 		/// </summary>
-		public static string BuildInfo = "1.2.6.4 - SCL BETA, Build 1";
+		public static string BuildInfo = "1.2.6.4 - SCL BETA, Build 2";
 
 
 		/// <summary>
