85b11f40cf8f248e14e2a020367a77e429627f9b
Ingo Wolfgang <ingo.wolfgang@t-online.de>
2024-09-22 21:46:31 +0200

    Changing Mode on first startup of new version (removing stutterfix)

--- Globals.cs
+++ Globals.cs
@@ -735,6 +735,9 @@ namespace Project_127
 
                 if (Settings.LastLaunchedVersion < new Version("1.4.0.0"))
                 {
+                    Settings.P127Mode = "default";
+                    Settings.DMMode = "default";
+
                     string tmp = HelperClasses.RegeditHandler.GetValue("DownloadManagerInstalledSubassemblies");
                     tmp = tmp.Replace(@"\\\\", @"\\");
                     HelperClasses.RegeditHandler.SetValue("DownloadManagerInstalledSubassemblies", tmp);
